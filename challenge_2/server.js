5af44693c285e85f404c1bc380704b06bc55986e
Chris Risden <christopherrisden@gmail.com>
2021-08-04 20:18:28 -0700

    fixed my server to use post, added a button for submit

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,10 +2,20 @@
 var Express = require('express');
 const app = Express();
 const port = 3000;
+app.use(Express.static('client'));
+
 /*
+client-server app that converts json to csv
+
 The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
 You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
 You may also assume that child records in the JSON will always be in a property called `children`.
+
+the client must be able to submit json data to the server, and the server renders that into csv, and sends it back to the client
+
+can use jquery on client, can use bodyparser for express middleware
+
+
 
 */
 
@@ -16,10 +26,10 @@ app.get('/', (req, res) => {
   res.end('test /');
 });
 
-app.get('/generate', (req, res) => {
-  console.log('get /generate');
+app.post('/generate', (req, res) => {
+  console.log('post /generate');
   res.statusCode = 200;
-  res.end('test /generate');
+  res.end('test post /generate');
 });
 
 
